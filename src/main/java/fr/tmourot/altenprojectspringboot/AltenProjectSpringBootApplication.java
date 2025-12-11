de6e9eb2534e347ce589f5a0087b62f3c90b3b0c
tmourot <tmourot@live.fr>
2025-12-11 12:58:26 +0100

    first commit

---
+++ src/main/java/fr/tmourot/altenprojectspringboot/AltenProjectSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fr.tmourot.altenprojectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltenProjectSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AltenProjectSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/tmourot/altenprojectspringboot/AltenProjectSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.tmourot.altenprojectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltenProjectSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
